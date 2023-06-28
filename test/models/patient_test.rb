2069f7e7f36b3eb26580403ef8445cce0900217a
Lucas Branco Alves de Melo <lucasbranco027@protonmail.com>
2023-06-28 12:26:26 -0300

    patient test added.

--- test/models/patient_test.rb
+++ test/models/patient_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class PatientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "patient should not be created with empty variables" do
+    patient = Patient.new
+    assert_not patient.save, "Created patient with null variables"
+  end
+
+  test "patient should be created with not null and correct variables" do
+    patient = Patient.new(name: "Reinhardt", birth: Date.new(1927, 7, 27), cpf: "10598412523", email: "reinhardt@example.com")
+    assert patient.save
+  end
 end
