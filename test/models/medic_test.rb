ead4cf5baeb99e5cfad305fe9a91ef7448a97a9a
Lucas Branco Alves de Melo <lucasbranco027@protonmail.com>
2023-06-28 12:30:34 -0300

    medic test added.

--- test/models/medic_test.rb
+++ test/models/medic_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class MedicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :medics
+  test "Medic should not be created with empty variables" do
+    medic = Medic.new
+    assert_not medic.save, "Created with empty variables"
+  end
+
+  test "Medic should not be created with wrong cpf" do
+    medic = medics(:one)
+    medic.cpf = "123"
+    assert_not medic.save, "Created with wrong cpf"
+  end
 end
