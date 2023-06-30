06f3c54f3db81663bda4751a7c67e57f2fc3f9ca
Lucas Branco Alves de Melo <lucasbranco027@protonmail.com>
2023-06-30 13:24:32 -0300

    patient feature added.

---
+++ features/step_definitions/patients_steps.rb
@@ -0,0 +1,27 @@
+Given('Open the patient {string} page') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I fill all fields with valid information') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click on {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see that this patient was saved') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given('Open the patient page') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I click on the {string} link') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I see that this patient was deleted') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
